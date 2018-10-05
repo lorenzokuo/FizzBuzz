96b52d7777c765e13671f762a9553a93e2c1bfc5
Lorenzokuo <lorenzokuo@gmail.com>
2018-10-05 11:59:07 -0700

    first commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+	public static void main(String[] args) {
+		FizzBuzz iD = new FizzBuzz();
+		iD.fizzBuzz(3);
+		iD.fizzBuzz(5);
+		iD.fizzBuzz(15);
+		iD.fizzBuzz(2);
+		// String result = iD.fizzBuzz(3);
+		// System.out.println(result + " test");
+	}
+}
